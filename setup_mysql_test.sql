26daec9a763495dcb946817288ddcb0d2e0d345f
Nels <nelsfichera@gmail.com>
2021-08-09 17:49:35 -0400

    Fix name

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+--prepares mysql server, with user
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hb.b_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
