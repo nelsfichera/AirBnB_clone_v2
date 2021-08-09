6002814090893a8d6d8b375dcc06d0448963845d
Nels <nelsfichera@gmail.com>
2021-08-09 17:53:51 -0400

    comment syntax

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
---prepares mysql server, with user
+-- prepares mysql server, with user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
