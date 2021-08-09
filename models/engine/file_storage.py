7b0aac61492647405589bd5bb5f0ddca89370b1b
Nels <nelsfichera@gmail.com>
2021-08-09 18:46:49 -0400

    Permissions, reload fix, typo fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,17 +41,11 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+            with open(FileStorage.__file_path, 'r', encoding="UTF-8") as f:
+                for key, val in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
         except FileNotFoundError:
             pass
 
