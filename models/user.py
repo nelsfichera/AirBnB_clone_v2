de4341bef0aacef47828267dd6e238f90464c7b4
Nels <nelsfichera@gmail.com>
2021-08-10 15:24:32 -0400

    8 user

--- models/user.py
+++ models/user.py
@@ -2,11 +2,23 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv as env
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    if env("HBNB_TYPE_STORAGE") == db:
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        kwargs = {"cascade": "all, delete-orphan", "backref": "user"}
+        places = relationship("Place", **kwargs)
+        reviews = relationship("Review", **kwargs)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
