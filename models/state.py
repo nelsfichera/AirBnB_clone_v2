69e746397eac1e71aa56522f4876bd5bdaa6c515
Carson Stearn <cstearn@gmail.com>
2021-08-11 03:52:00 +0000

    table typo

--- models/state.py
+++ models/state.py
@@ -8,7 +8,7 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "cities"
+    __tablename__ = "states"
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
