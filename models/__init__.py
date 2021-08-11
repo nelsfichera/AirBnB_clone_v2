4faba5ea1760a2d7163200efb57f27d036f71e1f
Carson Stearn <cstearn@gmail.com>
2021-08-11 01:03:47 +0000

    typos

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv as env
 
-if env("HBNB_TYPE_STORAGE") = "db":
+if env("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
     storage = FileStorage()
--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, Integer, Float, ForiegnKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy import Table
 from sqlalchemy.orm import relationship
 from os import getenv as env
