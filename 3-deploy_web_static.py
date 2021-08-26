a582437856057ed0982e3bf873f0a39ae81de881
nels <72285348+nelsfichera@users.noreply.github.com>
2021-08-26 10:21:41 -0400

    style

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,12 +4,13 @@ from datetime import datetime
 from fabric.api import local, put, run, env
 from os import path
 from os.path import exists
-
 env.hosts = ['ubuntu@104.196.35.45', 'ubuntu@34.73.125.171']
+
 
 def deploy():
     '''deploys fabric methods'''
     return(do_deploy(do_pack()))
+
 
 def do_pack():
     '''packs contents of /webstatic into .tgz'''
@@ -21,9 +22,10 @@ def do_pack():
     file_name = "versions/web_static_{}.tgz".format(time)
     try:
         local("tar -cvzf {} web_static".format(file_name))
-	return(file_name)
+        return(file_name)
     except Exception:
         return(None)
+
 
 def do_deploy(archive_path):
     '''deploys content of web_static file to servers'''
@@ -31,7 +33,7 @@ def do_deploy(archive_path):
         return(False)
     try:
         archive_name = archive_path.split("/")[1].split(".")[0]
-	put(archive_path, "/tmp/")
+        put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}".format(archive_name))
         run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}"
             .format(archive_name, archive_name))
@@ -39,10 +41,10 @@ def do_deploy(archive_path):
         run("mv /data/web_static/releases/{}/web_static/* "
             "/data/web_static/releases{}/"
             .format(archive_name, archive_name))
-	run("rm -rf /data/web_static/releases/{}/web_static")
+        run("rm -rf /data/web_static/releases/{}/web_static")
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{} /data/web_static/current"
             .format(archive_name))
-	return (True)
+        return (True)
     except Exception:
         return (False)
